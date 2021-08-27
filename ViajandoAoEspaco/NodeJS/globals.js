9b12eecfd9c93c4593ea3610db368d8e7de411ce
Savio Nascimento <savio.nascimento@tbdc.com.br>
2021-08-27 17:26:08 -0400

    ✨ New module Node - four first class

---
+++ ViajandoAoEspaco/NodeJS/globals.js
@@ -0,0 +1,5 @@
+// Diretório loca, onde está o arquivo que estamos executando
+console.log(__dirname)
+
+// Caminho completo + nome do arquivo
+console.log(__filename)
---
+++ ViajandoAoEspaco/NodeJS/require.js
@@ -0,0 +1,9 @@
+// Módulos nativos
+// const path = require('path');
+
+// console.log(path)
+
+
+// Meus Módulos 
+const myModule = require('./exports')
+console.log(myModule)
