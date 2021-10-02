a1a863e26452b1e46374fb3fc3bdd99a635f22b2
savionascimentodev <savionascimentodev@gmail.com>
2021-10-02 18:15:40 -0400

    ⚡️ data manipulation completed

--- GuiasEstelares/JavaScript/ManipulandoDados/manipulandoArrays.js
+++ GuiasEstelares/JavaScript/ManipulandoDados/manipulandoArrays.js
@@ -1,6 +1,27 @@
 // Manipulando Arrays
 
-// Criar array com construtor;
+let techs = ['html', 'css', 'js']
 
-let myArray = new Array('a', 'b', 'c', 'd')
-console.log(myArray);
+// Adicionar um item no fim;
+techs.push('VueJs');
+
+// Adicionar no começo;
+techs.unshift('algoritmos');
+
+// Remover do fim
+techs.pop()
+
+// Remover do começo;
+techs.shift()
+
+// Pegar somente alguns elementos do array;
+// console.log(techs.slice(1, 3))
+
+// Remover 1 ou mais items em qualquer posição do array;
+// techs.splice(1, 2)
+
+// Encontrar a posição de um elemento no array;
+let index = techs.indexOf('css')
+techs.splice(index, 1) // Busca e já remove o elemento;
+
+console.log(index)
